af922fb601fc502daf1e466435f27db4ee48ba1b
aaronv <aarongeovanny70@gmail.com>
2026-05-27 00:15:24 -0600

    Add template parsing to decoy

--- decoy.go
+++ decoy.go
@@ -1,25 +1,28 @@
 package decoy
 
 import (
+	"bytes"
+	"html/template"
+	"io"
 	"math/rand/v2"
+	"os"
 	"sync/atomic"
 	"time"
 
 	"github.com/aaron70/decoy/internal/random"
 )
 
-
 type Decoy struct {
-	Rand *rand.Rand
-	randomText *random.RandomText
+	Rand            *rand.Rand
+	randomText      *random.RandomText
 	intIncrementals map[string]*atomic.Int64
 }
 
 func NewDecoy(source rand.Source) (*Decoy, error) {
 	rand := rand.New(source)
 	d := &Decoy{
-		Rand: rand,
-		randomText: random.NewRandomText(rand, 6),
+		Rand:            rand,
+		randomText:      random.NewRandomText(rand, 6),
 		intIncrementals: make(map[string]*atomic.Int64),
 	}
 	return d, nil
@@ -54,4 +57,72 @@ func (d *Decoy) NextIncrementalInt(id string, start, step int) int64 {
 
 func (d *Decoy) RandomChoiceAny(choices ...any) any {
 	return RandomChoice(d, choices...)
+}
+
+type parseTemplateOption func(*parseTemplateConfig) error
+
+type parseTemplateConfig struct {
+	Name  string
+	Funcs template.FuncMap
+	Data  any
+}
+
+func WithTemplateNamed(name string) parseTemplateOption {
+	return func(ptc *parseTemplateConfig) error {
+		ptc.Name = name
+		return nil
+	}
+}
+
+func WithData(data any) parseTemplateOption {
+	return func(ptc *parseTemplateConfig) error {
+		ptc.Data = data
+		return nil
+	}
+}
+
+func WithFuncMap(funcs template.FuncMap) parseTemplateOption {
+	return func(ptc *parseTemplateConfig) error {
+		ptc.Funcs = funcs
+		return nil
+	}
+}
+
+func (d *Decoy) DefaultTemplateFuncMaps() template.FuncMap {
+	return template.FuncMap{
+		"RandomInt":      d.RandomInt,
+		"RandomChoice":   d.RandomChoiceAny,
+		"RandomText":     d.RandomText,
+		"NextIncrementalInt": d.NextIncrementalInt,
+		"EnvVariable":    os.Getenv,
+	}
+}
+
+func (d *Decoy) ParseTemplate(w io.Writer, tmpl string, options ...parseTemplateOption) error {
+	config := &parseTemplateConfig{
+		Funcs: d.DefaultTemplateFuncMaps(),
+	}
+
+	for _, option := range options {
+		if err := option(config); err != nil {
+			return err
+		}
+	}
+
+	t, err := template.New(config.Name).
+		Funcs(config.Funcs).
+		Parse(tmpl)
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, config.Data)
+}
+
+func (d *Decoy) ParseTemplateString(tmpl string, options ...parseTemplateOption) (string, error) {
+	str := bytes.NewBufferString("")
+	err := d.ParseTemplate(str, tmpl, options...)
+	if err != nil {
+		return "", err
+	}
+	return str.String(), nil
 }
