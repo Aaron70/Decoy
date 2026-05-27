156443697fde0087f430b8d38d06b7048a3a92d2
aaronv <aarongeovanny70@gmail.com>
2026-05-26 23:51:09 -0600

    Add Incremental int

--- cmd/main.go
+++ cmd/main.go
@@ -17,4 +17,8 @@ func main() {
 	Panic(err)
 
 	fmt.Printf("Random text: %q\n", d.RandomText(10))
+	fmt.Printf("Incremental 1: %d\n", d.NextIncrementalInt("id", 1, 1))
+	fmt.Printf("Incremental 2: %d\n", d.NextIncrementalInt("id", 1, 1))
+	fmt.Printf("Incremental 3: %d\n", d.NextIncrementalInt("id", 1, 1))
+	fmt.Printf("Incremental 4: %d\n", d.NextIncrementalInt("id", 1, 1))
 }
